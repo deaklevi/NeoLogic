5343fcf5c03dda3760a6fc0cb096209a33b7a6aa
deaklevi <deakleventevok@gmail.com>
2026-03-23 19:15:30 +0100

    Update mobile numbers

--- frontend/src/components/BaseAvailabilityDatas.vue
+++ frontend/src/components/BaseAvailabilityDatas.vue
@@ -7,7 +7,7 @@
             </div>
             <div>
                 <h3 class="text-lg font-bold text-gray-400">Email</h3>
-                <p class="text-sm">neo.logic@kft.com</p>
+                <p class="text-sm">neologicsupport@gmail.com</p>
             </div>
         </div>
 
@@ -16,10 +16,21 @@
                 <img src="../assets/telephone.png" alt="" class="w-6 h-6 object-contain">
             </div>
             <div>
-                <h3 class="text-lg font-bold text-gray-400">Telefon</h3>
-                <p class="text-sm">+36 1 123 4567</p>
+                <h3 class="text-lg font-bold text-gray-400">Horváth Gergő</h3>
+                <p class="text-sm"><a href="tel:+36704200637">+36 70 420 0637</a></p>
             </div>
         </div>
+
+        <div class="about-animate flex">
+            <div class="bg-[#222226] p-4 rounded-lg flex items-center justify-center mb-4 mr-5">
+                <img src="../assets/telephone.png" alt="" class="w-6 h-6 object-contain">
+            </div>
+            <div>
+                <h3 class="text-lg font-bold text-gray-400">Deák Levente</h3>
+                <p class="text-sm"><a href="tel:+36302365045">+36 30 236 5045</a></p>
+            </div>
+        </div>
+        
 
         <div class="about-animate flex">
             <div class="bg-[#222226] p-4 rounded-lg flex items-center justify-center mb-4 mr-5">
